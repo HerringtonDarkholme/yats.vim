6b42bdd3cd75df6c0dadc57f0e47ef5ef36de3b7
Herrington Darkholme <nonamesheep1@gmail.com>
2016-02-20 11:39:22 +0800

    add test file

---
+++ test/test.ts
@@ -0,0 +1,13 @@
+interface Promise<T> {
+  /*comment in interface*/
+}
+
+var p: Promise<void>
+
+[/*comment in array*/]
+
+function commentInFuncParam(/*test*/ a) {
+}
+
+const COMPILER_PROVIDERS: Array<any[]> = null
+
