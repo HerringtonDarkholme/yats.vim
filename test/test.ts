635f30a044d58a5aa5ff871d2b2d10a991c93372
Herrington Darkholme <HerringtonDarkholme@users.noreply.github.com>
2018-01-28 23:51:10 +0800

    clean up test

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,6 @@
-interface Promise<T> {
-  /*comment in interface*/
-}
-
 var p: Promise<void>
 
 [/*comment in array*/]
-
-function commentInFuncParam(/*test*/ a) {
-}
 
 const COMPILER_PROVIDERS: Array<any[]> = null
 
