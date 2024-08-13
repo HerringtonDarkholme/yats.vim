fc58cbc6f659929908c41b885194ee9ae093def3
Tobiasz Laskowski <tobil4sk@outlook.com>
2024-08-13 12:45:07 +0100

    Generate typescriptcommon.vim merged file as well
    This is how vim is organised now.

--- merge.js
+++ merge.js
@@ -2,6 +2,7 @@ var fs = require('fs')
 
 var entry = fs.readFileSync('syntax/typescript.vim', 'utf8')
 var entryReact = fs.readFileSync('syntax/typescriptreact.vim', 'utf8')
+var common = fs.readFileSync('syntax/common.vim', 'utf8')
 
 function replace(_, filename) {
   var source = fs.readFileSync(filename, 'utf8')
@@ -14,5 +15,7 @@ function importFile(source) {
 
 var merged = importFile(entry)
 var mergedReact = importFile(entryReact)
+var mergedCommon = importFile(common)
 fs.writeFileSync('merged/typescript.vim', merged)
 fs.writeFileSync('merged/typescriptreact.vim', mergedReact)
+fs.writeFileSync('merged/typescriptcommon.vim', mergedCommon)
